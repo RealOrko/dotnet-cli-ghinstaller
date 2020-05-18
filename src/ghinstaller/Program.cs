86be0e589359cf8dd09fad6a21fa8435a76716d8
RealOrko <>
2020-05-18 14:53:01 +0100

    Added stubs so I can start devleoping the installer client

---
+++ src/ghinstaller/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ghinstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("dotnet cli ghinstaller");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("   > ghi github.com cloundfoundry bosh-bootloader");
+        }
+    }
+}
