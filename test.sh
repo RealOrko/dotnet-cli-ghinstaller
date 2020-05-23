3a182ee6fe61944fee0d47941dc9cd6dd0018159
realorko <realorko@nowhere.com>
2020-05-23 13:47:13 +0100

    Wow go is a real pain ...

--- test.sh
+++ test.sh
@@ -43,7 +43,13 @@ cd ./build
 ./ghi download-release -o cloudfoundry -r bosh-bootloader -f v8.4.0 -a -af linux
 
 # Building a go app
-sudo apt install -y golang
+mkdir -p ./tmp
+cd ./tmp
+wget https://dl.google.com/go/go1.10.4.linux-amd64.tar.gz
+sudo tar -xvf go1.11.linux-amd64.tar.gz
+sudo mv go /usr/local
+cd ..
+
 ./ghi download-release -o gobuffalo -r packr -f v2.7.1 -t
 ./ghi untar -t v2.7.1.tar -o ./v2.7.1
 ./ghi build-go -a get -d ./v2.7.1/gobuffalo-packr-4b4a3c4/v2/packr2/ || true
